b2ce55869cf508e30848825dbe011a86ac491e67
goncalomotaribeiro <goncalomota.cr@gmail.com>
2021-01-17 00:50:28 +0000

    ss

--- p02_cluedo.js
+++ p02_cluedo.js
@@ -993,24 +993,7 @@ function createStairs(){
 }
 
 function cluedoLetters(){
-    const loader = new THREE.FontLoader();
-
-    loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-
-        const geometry = new THREE.TextGeometry( 'Hello three.js!', {
-            font: font,
-            size: 80,
-            height: 5,
-            curveSegments: 12,
-            bevelEnabled: true,
-            bevelThickness: 10,
-            bevelSize: 8,
-            bevelOffset: 0,
-            bevelSegments: 5
-        } );
-        geometry.position.set(0,0,0)
-        scene.add(geometry)
-    } );
+ 
 }
 
 
